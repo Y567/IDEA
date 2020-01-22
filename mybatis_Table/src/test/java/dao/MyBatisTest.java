bf86bd0b226271c2da104bcb5b8c1b7df6e2130f
Gyy <1592221415@qq.com>
2020-01-22 18:37:10 +0800

    mybatis中一对一的封装 一个用户有多个账户，一个账户属于一个用户 在账户的实体类中拥有用户属性， 利用resultMap对其进行封装 并进行测试

---
+++ mybatis_Table/src/test/java/dao/MyBatisTest.java
@@ -0,0 +1,81 @@
+package dao;
+
+import domain.Account;
+import domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class MyBatisTest {
+    private InputStream in;
+    private SqlSession sqlSession;
+    private UserDao userDao;
+    private AccountDao accountDao;
+
+    @Before  //表示在测试方法之前执行
+    public void init() throws IOException {
+        //1.加载配置文件获取字节输入流
+        in = Resources.getResourceAsStream("SqlMapConfig.xml");
+        //2.获取sqlSessionFactory工厂对象
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+        //3.获取SqlSession对象
+        sqlSession = factory.openSession();
+        //4.获取代理对象
+        userDao = sqlSession.getMapper(UserDao.class);
+        accountDao = sqlSession.getMapper(AccountDao.class);
+    }
+
+    @After
+    public void release() throws IOException {
+
+        //事务提交
+        sqlSession.commit();
+        //6.释放资源
+        if(sqlSession != null){
+            sqlSession.close();
+        }
+        if(in != null){
+            in.close();
+        }
+    }
+
+    @Test
+    public void userFindAll() throws IOException {
+        //5.执行方法
+        List<User> users = userDao.findAll();
+        for(User u: users){
+            System.out.println(u);
+        }
+    }
+
+    @Test
+    public void userFindById(){
+        User user = userDao.findById(5);
+        System.out.println(user);
+    }
+
+
+    @Test
+    public void accountFindAll() throws IOException {
+        //5.执行方法
+        List<Account> accounts = accountDao.findAll();
+        for(Account u: accounts){
+            System.out.println(u);
+        }
+    }
+
+    @Test
+    public void accountFindById(){
+        Account account = accountDao.findById(5);
+        System.out.println(account);
+    }
+
+}
