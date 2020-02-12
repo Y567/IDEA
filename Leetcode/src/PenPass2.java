fe9755fa3e587098137d941b4e7b4bfcf81bcc03
Y567 <1592221415@qq.com>
2020-02-12 17:16:27 +0800

    牛客练习 输出前K个最小的数字

---
+++ Leetcode/src/PenPass2.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PenPass2 {
+    /**
+     * 输出前K个最小的数字
+     */
+    public static ArrayList<Integer> GetLeastNumbers_Solution(int[] input, int k) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Arrays.sort(input);
+        for(int i = 0;i < k;i++){
+            list.add(input[i]);
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+        int[] input = {4,5,1,6,2,7,3,8};
+        System.out.println(GetLeastNumbers_Solution(input,4));
+    }
+}
