c6b2150afe7cc8c43198a9744434a343e549d9b5
Y567 <1592221415@qq.com>
2020-02-14 22:54:05 +0800

    牛客练习 教训：记得加空指针和异常判断，这有可能是导致不通过的原因。

--- Leetcode/src/PenPass2.java
+++ Leetcode/src/PenPass2.java
@@ -6,6 +6,9 @@ public class PenPass2 {
      * 输出前K个最小的数字
      */
     public static ArrayList<Integer> GetLeastNumbers_Solution(int[] input, int k) {
+        if(input == null || k > input.length){
+            return new ArrayList<>();
+        }
         ArrayList<Integer> list = new ArrayList<>();
         Arrays.sort(input);
         for(int i = 0;i < k;i++){
@@ -15,6 +18,6 @@ public class PenPass2 {
     }
     public static void main(String[] args) {
         int[] input = {4,5,1,6,2,7,3,8};
-        System.out.println(GetLeastNumbers_Solution(input,4));
+        System.out.println(GetLeastNumbers_Solution(input,10));
     }
 }
