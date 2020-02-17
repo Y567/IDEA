38f5b67b01ab1752d7a9a746d285cc0cd1944cf4
Y567 <1592221415@qq.com>
2020-02-17 22:59:30 +0800

    分类功能的service层

---
+++ travel/src/main/java/cn/itcast/travel/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,22 @@
+package cn.itcast.travel.service.impl;
+
+import cn.itcast.travel.dao.CategoryDao;
+import cn.itcast.travel.dao.impl.CategoryDaoImpl;
+import cn.itcast.travel.domain.Category;
+import cn.itcast.travel.service.CategoryService;
+
+import java.util.List;
+
+public class CategoryServiceImpl implements CategoryService {
+
+    private CategoryDao categoryDao = new CategoryDaoImpl();
+
+    /**
+     * 查询所有分类
+     * @return
+     */
+    @Override
+    public List<Category> findAll() {
+        return categoryDao.findAll();
+    }
+}
