9a2f7361d18d47c93638dcff5478d556d074c784
Y567 <1592221415@qq.com>
2020-02-29 23:40:32 +0800

    匿名内部类

--- java基础解惑/src/内部类/Body.java
+++ java基础解惑/src/内部类/Body.java
@@ -15,6 +15,12 @@ public class Body {
             System.out.println("心脏跳动");
             System.out.println("小心脏");
         }
+
+        public class Inner{
+            public void inner(){
+                System.out.println("内部的内部");
+            }
+        }
     }
     private String name = "小心脏";
 
@@ -24,11 +30,24 @@ public class Body {
     public static void main(String[] args) {
         //直接调用内部类 [方式外点内]
         Body.Heart heart = new Body().new Heart();
-
         heart.hert();
-
         System.out.println("========================");
         //间接访问，通过外部类的方法去访问
         new Body().test();
+        System.out.println("--------------------------");
+
+        //内部的内部类使用
+        Body.Heart.Inner i = new Body().new Heart().new Inner();
+        i.inner();
+
+        //匿名内部类实现
+        Dog dog = new Dog() {
+            @Override
+            public void run() {
+                System.out.println("阿狗已经美丽你说的嘛偶像");
+            }
+        };
+        dog.run();
+        dog.run();
     }
 }
---
+++ java基础解惑/src/内部类/Dog.java
@@ -0,0 +1,5 @@
+package 内部类;
+
+public interface Dog {
+    void run();
+}
