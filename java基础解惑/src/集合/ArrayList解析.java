7e7564d16f2437aba457dfc6e2d2261201631d94
Y567 <1592221415@qq.com>
2020-03-19 22:28:02 +0800

    集合相关

---
+++ java基础解惑/src/集合/ArrayList解析.java
@@ -0,0 +1,11 @@
+package 集合;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ArrayList解析 {
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();//JDK7中默认构造10个，JDK8懒加载，先不给大小
+        LinkedList<Object> objects = new LinkedList<>();
+    }
+}
