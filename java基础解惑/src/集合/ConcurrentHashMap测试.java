2d5cbf6583ea24968e83b0ecd7f09bd259122355
Y567 <1592221415@qq.com>
2020-03-24 22:21:09 +0800

    concurrentHashMap的小测试

---
+++ java基础解惑/src/集合/ConcurrentHashMap测试.java
@@ -0,0 +1,10 @@
+package 集合;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentHashMap测试 {
+    public static void main(String[] args) {
+        ConcurrentHashMap<Object, Object> objectObjectConcurrentHashMap = new ConcurrentHashMap<>();
+        objectObjectConcurrentHashMap.put(1,2);
+    }
+}
