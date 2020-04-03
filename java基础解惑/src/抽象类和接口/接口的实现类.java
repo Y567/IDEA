05e057610196af680bbe6b3f5ccb8664bdea7242
Y567 <1592221415@qq.com>
2020-04-03 23:58:44 +0800

    子类如果不重写父接口的default方法就会调用无反应，也不报错，但是重写就可以应用了

--- java基础解惑/src/抽象类和接口/接口的实现类.java
+++ java基础解惑/src/抽象类和接口/接口的实现类.java
@@ -2,12 +2,14 @@ package 抽象类和接口;
 
 public class 接口的实现类 implements 接口的默认方法,接口的默认方法2{
     public static void main(String[] args) {
-        接口的默认方法.test2();
+//        接口的默认方法.test2();
         new 接口的实现类().test1();
     }
 
     @Override
     public void test1() {
         //如果实现类实现的两个接口有相同名字的默认方法需要重写这个default方法
+        System.out.println("重写了重名的default方法");
+
     }
 }
