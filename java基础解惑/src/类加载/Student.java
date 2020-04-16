c6e25072ea0dfae88d623c2995e32f3b25dc0b28
Y567 <1592221415@qq.com>
2020-04-16 21:54:09 +0800

    打印类加载器

--- java基础解惑/src/类加载/Student.java
+++ java基础解惑/src/类加载/Student.java
@@ -12,7 +12,5 @@ public class Student {
         System.out.println("加载该类的加载器是"+classLoader);
         System.out.println("appClassLoader的父类加载器"+classLoader.getParent());  //扩展类加载器
         System.out.println("appClassLoader的父类的父类加载器"+classLoader.getParent().getParent());//启动类加载器没权限获取到
-
     }
-
 }
